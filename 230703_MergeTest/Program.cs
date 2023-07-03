f91b3760f3001c93488c1cb9bb0162cb662ea8bd
DESKTOP-EDE2U79\KGA <jahniyoon@gmail.com>
2023-07-03 14:47:17 +0900

    [jahni] add Ruler

--- 230703_MergeTest/Program.cs
+++ 230703_MergeTest/Program.cs
@@ -12,6 +12,25 @@ namespace _230703_MergeTest
         {
 
             Console.WriteLine("Hello World!");
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+
+        }
+        public class Ruler
+        {
+            private const float ONE_INCH = 2.54F;
+            public int Centimeter { get; set; } = 0;
+            public float Inch
+            {
+                get { return Centimeter * ONE_INCH; }
+                private set { Centimeter = (int)(value / ONE_INCH); }
+            }
+
+            public Ruler(int cmValue) { Centimeter = cmValue; }
+            public void Run()
+            {
+                Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다 ");
+            }
 
         }
     }
