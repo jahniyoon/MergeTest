443ba025e78266e1f2b5d5b5d850244879ce75e3
DESKTOP-EDE2U79\KGA <jahniyoon@gmail.com>
2023-07-03 17:31:39 +0900

    style. Modified class position

--- 230703_MergeTest/Program.cs
+++ 230703_MergeTest/Program.cs
@@ -6,33 +6,16 @@ using System.Threading.Tasks;
 
 namespace _230703_MergeTest
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            // {    2023. 07. 03. Add user input / Beta
-            string userInput = string.Empty;
-            Console.WriteLine("This program convert Cm to Inch");
-            Console.WriteLine("Input Cm value : ");
-            userInput = Console.ReadLine();
-
-            int cmInput = 0;
-            int.TryParse(userInput, out cmInput);
-
-            Ruler ruler = new Ruler(cmInput);
-            // {    2023. 07. 03. Add user input / Beta
-            ruler.Run();
-        }
+   
         public class Ruler
         {
+            // 2023. 07. 03. Modified class position / Alpha
             private const float ONE_INCH = 2.54F;
             public int Centimeter { get; set; } = 0;
             public float Inch
             {
                 get { return Centimeter * ONE_INCH; }
-
-                // {2023. 07. 03        Add private SetInch function / Gamma
-                private set { this._SetInch(value); }
+                private set { Centimeter = (int)(value / ONE_INCH); }
             }
 
             public Ruler(int cmValue) { Centimeter = cmValue; }
@@ -41,9 +24,21 @@ namespace _230703_MergeTest
                 Console.WriteLine($"{this.Centimeter}cm 는 {this.Inch}inch 입니다 ");
             }
 
-            private void _SetInch(float inchvalue) { Centimeter = (int)(inchvalue / ONE_INCH); }
-                // {2023. 07. 03        Add private SetInch function / Gamma
+        }
+        // 2023. 07. 03. Modified class position / Alpha
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Hello World!");
+                Ruler ruler = new Ruler(10);
+                ruler.Run();
+
+            }
 
         }
-    }
+
+ 
 }
